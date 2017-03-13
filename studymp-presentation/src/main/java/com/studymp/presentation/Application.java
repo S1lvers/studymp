35b8ab06e7d9ffae541eb1107ff82cb124b73c3b
Антон Грибушка <anton.gribushka@gmail.com>
2017-03-13 17:52:25 +0300

    Сделана архитектура приложения, собрал помник настроил properties

---
+++ studymp-presentation/src/main/java/com/studymp/presentation/Application.java
@@ -0,0 +1,14 @@
+package com.studymp.presentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by qwerty on 13.03.2017.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
