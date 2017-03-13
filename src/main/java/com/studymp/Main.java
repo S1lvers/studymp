d0796f4dc05a49bfa905ab9198b4c1630bb7f834
Антон Грибушка <anton.gribushka@gmail.com>
2017-03-13 13:02:49 +0300

    Hello world!

---
+++ src/main/java/com/studymp/Main.java
@@ -0,0 +1,10 @@
+package com.studymp;
+
+/**
+ * Created by qwerty on 13.03.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
