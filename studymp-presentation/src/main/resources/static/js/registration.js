6b061eb81d0eefeb66afe040355553b89066cdcb
Nikita Malkov <malkov.mirea@yandex.ru>
2017-03-22 16:39:44 +0300

    WIP

--- studymp-presentation/src/main/resources/static/js/registration.js
+++ studymp-presentation/src/main/resources/static/js/registration.js
@@ -1,23 +1,15 @@
 /**
  * Created by malko on 20.03.2017.
  */
-$('#reg-on-studymp').click(function (e) {
-    var confirmPassword = $('input[id=password2]').val();
+$('#registration-form').submit(function(e) {
+    e.preventDefault();
     var formData = {
         'username': $('input[id=username]').val(),
         'email': $('input[id=email]').val(),
         'password': $('input[id=password]').val(),
     };
-    if (formData.username.length < 5) {
-        $('#error_wrapper').replaceWith('<textarea id="error_wrapper"> Имя пользователя должно быть не менее 5 символов </textarea>');
-        return;
-    } else if (formData.password.length < 8) {
-        $('#error_wrapper').replaceWith('<textarea id="error_wrapper"> Пароль должен быть не менее 8 символов </textarea>');
-        return;
-    } else if (formData.password !== confirmPassword) {
-        $('#error_wrapper').replaceWith('<textarea id="error_wrapper"> Пароли не совпадают </textarea>');
-        return;
-    }
+
+
     $.ajax({
         type: "PUT",
         contentType: "application/json",
@@ -27,6 +19,4 @@ $('#reg-on-studymp').click(function (e) {
     }).done(function (data) {
         console.log(data);
     });
-
-
 });
