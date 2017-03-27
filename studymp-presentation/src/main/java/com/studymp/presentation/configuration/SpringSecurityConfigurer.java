cc704ccdde6c51bc14c72a3f861932d92e01d7f2
Nikita Malkov <malkov.mirea@yandex.ru>
2017-03-27 16:33:28 +0300

    фикс

--- studymp-presentation/src/main/java/com/studymp/presentation/configuration/SpringSecurityConfigurer.java
+++ studymp-presentation/src/main/java/com/studymp/presentation/configuration/SpringSecurityConfigurer.java
@@ -32,7 +32,7 @@ public class SpringSecurityConfigurer extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                .antMatchers("/api/auth*", "/registration.html").permitAll()
+                .antMatchers("/api/auth/**", "/registration.html").permitAll()
                 .antMatchers("/user*").hasAuthority("USER")
                 .antMatchers("/admin*").hasAuthority("ADMIN")
                 .anyRequest().authenticated()
