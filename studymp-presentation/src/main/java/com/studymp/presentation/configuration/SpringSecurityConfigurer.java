bf8c8baaaf8d4b1a4f20e98ebe53f5714652e2b7
Nikita Malkov <malkov.mirea@yandex.ru>
2017-03-29 15:15:46 +0300

    фикс

--- studymp-presentation/src/main/java/com/studymp/presentation/configuration/SpringSecurityConfigurer.java
+++ studymp-presentation/src/main/java/com/studymp/presentation/configuration/SpringSecurityConfigurer.java
@@ -32,7 +32,7 @@ public class SpringSecurityConfigurer extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                .antMatchers("/api/auth/**", "/registration.html").permitAll()
+                .antMatchers("/api/auth*", "/registration.html").permitAll()
                 .antMatchers("/").hasAuthority("USER")
                 .antMatchers("/admin*").hasAuthority("ADMIN")
                 .anyRequest().authenticated()
@@ -40,7 +40,7 @@ public class SpringSecurityConfigurer extends WebSecurityConfigurerAdapter{
                 .formLogin().loginPage("/login.html").permitAll()
                 .and()
                 .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login.html");
-        /*http.csrf().disable().authorizeRequests().anyRequest().permitAll();*/
+        http.csrf().disable().authorizeRequests().anyRequest().permitAll();
     }
 
 
