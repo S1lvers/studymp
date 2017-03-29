6f5300631c0139bff4c915fbd82522fdbe35357e
Антон Грибушка <anton.gribushka@gmail.com>
2017-03-29 18:45:07 +0300

    Исправлена ошибка с css, теперь подключается

--- studymp-presentation/src/main/java/com/studymp/presentation/configuration/WebMvcConfiguration.java
+++ studymp-presentation/src/main/java/com/studymp/presentation/configuration/WebMvcConfiguration.java
@@ -2,6 +2,7 @@ package com.studymp.presentation.configuration;
 
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -13,5 +14,12 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login.html").setViewName("login");
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (!registry.hasMappingForPattern("/resources/**")) {
+            registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+        }
     }
 }
