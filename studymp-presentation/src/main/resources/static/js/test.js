3bae4c373bd16d7acf1e4068037adea96649e046
Nikita Malkov <malkov.mirea@yandex.ru>
2017-04-14 19:32:01 +0300

    сменил для себя юзернейм

--- studymp-presentation/src/main/resources/static/js/test.js
+++ studymp-presentation/src/main/resources/static/js/test.js
@@ -2,10 +2,10 @@ $('#testAjax').click(function (e) {
     $.ajax({
         type : "GET",
         contentType : "application/json",
-        url : "/user/chat?destination=username1",
+        url : "/user/chat?destination=username2",
         dataType : 'json',
         timeout : 100000
-    }).success(function (data) {
+    }).always(function (data) {
         data.console.log()
     });
 });
