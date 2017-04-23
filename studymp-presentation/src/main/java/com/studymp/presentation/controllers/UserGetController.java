e8f90ae704c9efcd9ac9a351a5baa024a8b3b61d
Антон Грибушка <anton.gribushka@gmail.com>
2017-04-23 18:35:41 +0300

    Small fixes

--- studymp-presentation/src/main/java/com/studymp/presentation/controllers/UserGetController.java
+++ studymp-presentation/src/main/java/com/studymp/presentation/controllers/UserGetController.java
@@ -12,6 +12,7 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by qwerty on 29.03.2017.
@@ -42,6 +44,10 @@ public class UserGetController {
     public String getUsers(Model model){
         try {
             List<User> users = userService.findAll();
+            //without us
+            List<User> result = users.stream()
+                    .filter(line -> !SecurityContextHolder.getContext().getAuthentication().getName().equals(line.getUsername()))
+                    .collect(Collectors.toList());
             model.addAttribute("users", users);
         } catch (Exception e){
             LOGGER.error("Не удалось получить список пользователей");
